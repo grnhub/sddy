d9b071cc2650b3e5444f89e44e41b0f3e5a5f237
grnhub <48713266+grnhub@users.noreply.github.com>
2019-11-11 17:57:00 +0900

    믜칭거아입니까

--- src/screens/PostProductScreen.js
+++ src/screens/PostProductScreen.js
@@ -1,11 +1,29 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, Image, TouchableOpacity, Alert, ScrollView, PickerIOSComponent} from 'react-native';
+import {StyleSheet, Text, View, Image, TouchableOpacity, Alert, ScrollView, Picker} from 'react-native';
 
 export default class PostProductScreen extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+          category: ''
+        };
+    }
+
     render() {
         return(
             <View>
-                <Text>카테고리 셀렉트리스트 </Text>
+                <Picker
+                    selectedValue={this.state.category}
+                    style={{height: 50, width: 100}}
+                    onValueChange={(itemValue, itemIndex) =>
+                        this.setState({category: itemValue})
+                    }>
+                    <Picker.Item label="디지털/가전" value="1" />
+                    <Picker.Item label="유아용품" value="2" />
+                    <Picker.Item label="운동기기" value="3" />
+                    <Picker.Item label="뷰티/미용" value="4" />
+                    <Picker.Item label="기타" value="5" />
+                </Picker>
                 <Text>상품이름 입력</Text>
                 <Text>대여기간</Text>
                 <Text>달력API 가져오기</Text>
@@ -14,7 +32,7 @@ export default class PostProductScreen extends Component {
                 <Text>직접거래/택배거래</Text>
                 <Text>상품내용입력</Text>
                 <Text>사진첨부</Text>
-                
+
             </View>
         )
     }
