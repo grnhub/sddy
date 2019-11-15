c9a28498eff6e0420eadd681adaccdac1e8005e7
minicks <53505096+minicks@users.noreply.github.com>
2019-11-15 16:42:20 +0900

    Update EvaluateScreen.js

--- src/screens/EvaluateScreen.js
+++ src/screens/EvaluateScreen.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, TextInput, KeyboardAvoidingView,Image } from 'react-native';
-import { Rating, AirbnbRating } from 'react-native-elements';
+import { Rating, AirbnbRating,Button } from 'react-native-elements';
 import { Container, Header, Content, DatePicker,Textarea, Form } from 'native-base';
 import historyCss from '../css/HistoryStyle';
-export default class EvaluateScreen extends Component {
+import Stars from 'react-native-stars';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+export default class Test extends Component {
  
     constructor(props) {
         super(props);
+
+        console.log("------------------------------ Test");
+        this.getProductList();
     }
 
     selectStar(index) {
@@ -19,10 +25,12 @@ export default class EvaluateScreen extends Component {
         )
     }
 
+    ///api/commentDapp post dapp_name=(string)&author=(string)&pwd=(string)&parent_author=(string)&parent_permlink=(string)&permlink=(string)&title=(string)&body=(string)&json_meta=(string)
+
     render() {
         return (
             <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
-                <View style={{flex:3, 
+                <View style={{flex:2, 
                     flexDirection:"row",
                     marginBottom: 5,
                     paddingBottom: 5,
@@ -31,7 +39,7 @@ export default class EvaluateScreen extends Component {
                     paddingRight: 10,
                     paddingLeft: 10,
                     fontSize: 20}}> 
-                    <View style={{flex: 2.5}}>
+                    <View style={{flex: 2}}>
                         <Image source={{uri: 'http://img.hani.co.kr/imgdb/resize/2018/0209/00500596_20180209.JPG'}} style={historyCss.image}></Image>
                     </View>
                     <View style={historyCss.namebox}>
@@ -42,21 +50,24 @@ export default class EvaluateScreen extends Component {
                 <View style={styles.content2}>
                     <Text style={styles.content2_text}>평가를 남기시면 SNAC을 드립니다.</Text>
                 </View>
-                <View style={[styles.rowflex, styles.content3]}>
+
+                <View style={{flex: 2,margin:8}}>
                     <Text>상품평가</Text>
-                    <AirbnbRating
-                    count={5}
-                    reviews={["Terrible", "Bad", "Good",  "Very Good",  "Amazing"]}
-                    ratingColor='#D4F4FA'
-                    defaultRating={0}
-                    size={50}
-                    ratingTextColor='#D4F4FA'
-                    ratingBackgroundColor='#D4F4FA'
-                    onPress={()=>console.log("=====")}
-                    />
+                    <View style={{alignItems:'center',borderWidth:1}}>
+                        <Stars
+                            default={2.5}
+                            count={5}
+                            half={true}
+                            starSize={1000} 
+                            fullStar={<MaterialCommunityIcons name={'star'} style={[styles.myStarStyle]}/>}
+                            emptyStar={<MaterialCommunityIcons name={'star-outline'} style={[styles.myStarStyle, styles.myEmptyStarStyle]}/>}
+                            halfStar={<MaterialCommunityIcons name={'star-half'} style={[styles.myStarStyle]}/>}
+                            style={{width:400, height:400}}
+                        />
+                    </View>   
                 </View>
                 
-                <View style={{flex: 2}}>
+                <View style={{flex: 2,margin:8}}>
                     <View><Text>상세평가</Text></View>
                     <Content>
                         <Form>
@@ -65,13 +76,29 @@ export default class EvaluateScreen extends Component {
                     </Content>
                 </View>
                 
-                <View style={styles.rowflex}>
-                    <TouchableOpacity style={[styles.button, {backgroundColor: '#cdcdcd'}]}>
-                        <Text>취소하기</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={[styles.button, {backgroundColor: '#4630eb', color: 'white'}]}>
-                        <Text>등록하기</Text>
-                    </TouchableOpacity>
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        justifyContent: 'space-around',
+                        padding: 10,}}>
+                    <Button
+                    buttonStyle={{alignContent: 'center',
+                    justifyContent: 'center',
+                    width: 130,
+                    backgroundColor: '#cdcdcd'}}
+                    onPress={() => console.log('aa')}
+                    
+                    title='취소하기'
+                    />
+    
+                    <Button
+                    buttonStyle={{alignContent: 'center',
+                    justifyContent: 'center',
+                    width: 130,
+                    backgroundColor: '#4630EB'}}
+                    onPress={() => console.log('bb')}
+                    title='등록하기' 
+                    />
                 </View>
             </KeyboardAvoidingView>
         )
@@ -84,7 +111,6 @@ const styles = StyleSheet.create(
             flex: 1,
             width: "100%",
             backgroundColor: 'white',
-            alignItems: 'center',
             paddingTop: 10,
             paddingBottom: 25
        },
@@ -146,6 +172,13 @@ const styles = StyleSheet.create(
             alignItems: 'center',
             textAlign: 'center',
             marginLeft:22
-       }
+       },
+       myStarStyle: {
+        color: 'yellow',
+        backgroundColor: 'transparent',
+      },
+      myEmptyStarStyle: {
+        color: 'white',
+      }
     }
 );
